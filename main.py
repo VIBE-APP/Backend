0541075c9f3e61a6715fd54f5f25ab54da394611
Alex Jabbour <redpod22@gmail.com>
2020-07-22 16:14:45 -0400

    template endpoints

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ app = Flask(__name__)
 
 # testing purposes
 @app.route('/')
-def hello_world():
-    return 'You've hit VIBE backend'
+def hello_world(): 
+    return "You've hit VIBE backend"
 
+# signing up a user
 @app.route('/signup')
 def signup_script():
-	pass
-	return 'OK'
+    pass
 
+# signing in user
+@app.route('/signin')
+def signin_script():
+    pass
+
+# get using profile info - for user profile page
+@app.route('/get_user_profile_info')
+def get_user_profile_info():
+    pass
